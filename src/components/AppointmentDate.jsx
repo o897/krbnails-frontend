5723c4d0275a7876b890f09039224e992084a513
o897 <orapelengm239@gmail.com>
2024-02-15 01:46:22 +0200

    update appointment date page css

--- src/components/AppointmentDate.jsx
+++ src/components/AppointmentDate.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { worktimes } from "../data";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const AppointmentDate = () => {
   const [appointmentDate, setAppointmentDate] = useState();
@@ -12,19 +15,20 @@ const AppointmentDate = () => {
   return (
     <div className="appointment__form-date">
       <form onSubmit={formSubmit()}>
-      <div className="date">
-        <input type="date" name="date" />
-      </div>
-      <div className="times">
+        <div className="date">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker />
+          </LocalizationProvider>
+        </div>
+        <div className="times">
           {worktimes.map((day) => (
             <div className="time" onClick={() => setAppointmentTime(day.time)}>
               {day.time}
             </div>
           ))}
-      </div>
-      
-            <button className="hero__bookbtn">BOOK AN APPOINTMENT</button>
-                
+        </div>
+
+        <button className="hero__bookbtn">BOOK AN APPOINTMENT</button>
       </form>
     </div>
   );
