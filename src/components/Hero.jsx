c8faa627cd6af28329c830d3b731d0a78dbfc989
o897 <orapelengm239@gmail.com>
2024-04-18 04:02:21 +0200

    add book link hero pg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,21 +16,22 @@ import { Link } from "react-router-dom";
 
 // data-aos="fade-up"
 function Hero() {
-
   return (
     <>
-    
       <section className="hero">
         <div className="hero__welcome">
-          <div className="hero__welcome-title">Elevate Your Look, One Nail at a Time</div>
-          <div className="hero__welcome-img">
-            <img src='https://www.refinery29.com/images/8448082.jpg' alt="landing img" />
+          <div className="hero__welcome-title">
+            Elevate Your Look, One Nail at a Time
           </div>
-          <Link to='book'>
+          <div className="hero__welcome-img">
+            <img
+              src="https://www.refinery29.com/images/8448082.jpg"
+              alt="landing img"
+            />
+          </div>
+          <Link to="book">
             <button className="hero__bookbtn">BOOK AN APPOINTMENT</button>
           </Link>
-
-          
         </div>
       </section>
 
@@ -38,32 +39,42 @@ function Hero() {
         <section className="services">
           <h2>Services</h2>
           <div className="services">
-            <div className="services__container services__container-big" data-aos='fade-right'>
+            <div
+              className="services__container services__container-big"
+              data-aos="fade-right"
+            >
               <img src={acrylicImg} alt="" srcSet="" />
               <div className="services__title">Acrylic</div>
               <div className="services__sub">4 Services</div>
-
             </div>
-            <div className="services__container services__container-small" data-aos='fade-left'>
+            <div
+              className="services__container services__container-small"
+              data-aos="fade-left"
+            >
               <img src={pedicureImg} alt="" srcSet="" />
               <div className="services__title">Pedicure</div>
               <div className="services__sub">2 Services</div>
-
             </div>
-            <div className="services__container services__container-big" data-aos='fade-right'>
+            <div
+              className="services__container services__container-big"
+              data-aos="fade-right"
+            >
               <img src={designImg} alt="" srcSet="" />
               <div className="services__title">Designs</div>
               <div className="services__sub">4 Services</div>
             </div>
-            <div className="services__container services__container-small" data-aos='fade-left'>
+            <div
+              className="services__container services__container-small"
+              data-aos="fade-left"
+            >
               <img src={homeImg} alt="" srcSet="" />
               <div className="services__title">Buff & Shine</div>
               <div className="services__sub">2 Services</div>
             </div>
             <div className="services__container">
-            <Link to='menu'>
-            <button className="services__container-btn">LEARN MORE</button>
-            </Link>
+              <Link to="menu">
+                <button className="services__container-btn">LEARN MORE</button>
+              </Link>
             </div>
           </div>
         </section>
@@ -79,58 +90,61 @@ function Hero() {
         <section>
           <div className="book">
             <div className="book__heading">Nail Your Look, Every Day!</div>
-            <button className="book__btn">BOOK AN APPOINTMENT</button>
+            <button className="book__btn">
+              <Link to="book">BOOK AN APPOINTMENT</Link>
+            </button>
           </div>
         </section>
         <section className="gallery">
-            <div className='gallery__heading'>Follow our journey</div>
-            <div className="gallery__container">
-              <img src={images[1].source} alt="" />
-              <img src={images[2].source} alt="" />
-            </div>
-            <div className="gallery__container">
-              <img src={images[3].source} alt="" />
-              <img src={images[4].source} alt="" />
-            </div>
-            <div className="gallery__container">
-              <img src={images[5].source} alt="" />
-              <img src={images[6].source} alt="" />
-            </div>
-            <div className="gallery__container">
-              <img src={images[7].source} alt="" />
-              <img src={images[0].source} alt="" />
-            </div>
+          <div className="gallery__heading">Follow our journey</div>
+          <div className="gallery__container">
+            <img src={images[1].source} alt="" />
+            <img src={images[2].source} alt="" />
+          </div>
+          <div className="gallery__container">
+            <img src={images[3].source} alt="" />
+            <img src={images[4].source} alt="" />
+          </div>
+          <div className="gallery__container">
+            <img src={images[5].source} alt="" />
+            <img src={images[6].source} alt="" />
+          </div>
+          <div className="gallery__container">
+            <img src={images[7].source} alt="" />
+            <img src={images[0].source} alt="" />
+          </div>
         </section>
         <section className="reviews">
           <div className="review__title">They're Talking</div>
-          <div className="review__container" data-aos='fade-left'>
+          <div className="review__container" data-aos="fade-left">
             <div className="review__container-review">
-            Positive vibes all the way. Hygienic, long-lasting gel manicure, and a friendly staff. Found my new favorite spot – definitely coming back!
+              Positive vibes all the way. Hygienic, long-lasting gel manicure,
+              and a friendly staff. Found my new favorite spot – definitely
+              coming back!
             </div>
             <div className="review__container-name">- Rearabilwe</div>
           </div>
-          <div className="review__container" data-aos='fade-down'>
-            <div className="review__container-review" >
-              Had an amazing nail art session. Creative team,
-              clean space, and a relaxing vibe. Definitely exceeded my
-              expectations. Highly recommend!
+          <div className="review__container" data-aos="fade-down">
+            <div className="review__container-review">
+              Had an amazing nail art session. Creative team, clean space, and a
+              relaxing vibe. Definitely exceeded my expectations. Highly
+              recommend!
             </div>
             <div className="review__container-name">Kearabilwe</div>
           </div>
-          <div className="review__container" data-aos='fade-down'>
+          <div className="review__container" data-aos="fade-down">
             <div className="review__container-review">
-              Absolutely loved my experience. Flawless manicure,
-              friendly staff, and a welcoming atmosphere. Can't wait to go back
-              for more pampering.
+              Absolutely loved my experience. Flawless manicure, friendly staff,
+              and a welcoming atmosphere. Can't wait to go back for more
+              pampering.
             </div>
             <div className="review__container-name">Tshepang</div>
           </div>
         </section>
-
       </main>
 
       <footer>
-      <section className="footer">
+        <section className="footer">
           <div className="footer__visit-tile">Visit Us</div>
           <div className="footer__container">
             <div className="footer__title">LOCATION</div>
@@ -144,7 +158,9 @@ function Hero() {
             <div className="footer__title">Timetable</div>
             <div className="footer__timetable">
               <div className="footer__details">Sunday: Closed</div>
-              <div className="footer__details">Monday - Friday : 10am - 18pm</div>
+              <div className="footer__details">
+                Monday - Friday : 10am - 18pm
+              </div>
               <div className="footer__details">Saturday : 10am - 18pm</div>
             </div>
           </div>
@@ -157,7 +173,9 @@ function Hero() {
               <FontAwesomeIcon icon={faTiktok} />
             </li>
             <li>
-              <a href="https://www.instagram.com/tlamis_nail_gallery/"><FontAwesomeIcon icon={faInstagram} /></a>
+              <a href="https://www.instagram.com/tlamis_nail_gallery/">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
             </li>
             <li>
               <FontAwesomeIcon icon={faWhatsapp} />
