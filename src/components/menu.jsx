6f4aa111011f5893642ae1a115a2c80d718a0c75
o897 <orapelengm239@gmail.com>
2024-04-18 16:17:17 +0200

    added menu header

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,8 +1,19 @@
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <>
+      <div className="bookform__header">
+        <Link to="/" style={{ color: "white" }}>
+          <span>
+            <FontAwesomeIcon className="angle-icon" icon={faAngleLeft} />
+          </span>
+        </Link>
+        Home
+      </div>
       <div className="menu">
         <div className="menu__heading">Tlami's Nail Gallery</div>
         <div className="menu__subheading">PRICELIST</div>
@@ -16,45 +27,33 @@ const Menu = () => {
               <div className="menu__pricelist-row--cost">R190 - R200</div>
             </div>
             <div className="menu__pricelist-row menu__bg-pink">
-              <div className="menu__pricelist-row--title">
-                Long
-              </div>
+              <div className="menu__pricelist-row--title">Long</div>
               <div className="menu__pricelist-row--cost">R250</div>
             </div>
             <div className="menu__pricelist-row">
-              <div className="menu__pricelist-row--title">
-                XL
-              </div>
+              <div className="menu__pricelist-row--title">XL</div>
               <div className="menu__pricelist-row--cost">R300</div>
             </div>
             <div className="menu__pricelist-row menu__bg-pink">
-              <div className="menu__pricelist-row--title">
-                CCL
-              </div>
+              <div className="menu__pricelist-row--title">CCL</div>
               <div className="menu__pricelist-row--cost">R350</div>
             </div>
             <div className="menu__pricelist-row  ">
-              <div className="menu__pricelist-row--title">
-                Acrylic Overlay
-              </div>
+              <div className="menu__pricelist-row--title">Acrylic Overlay</div>
               <div className="menu__pricelist-row--cost">R185</div>
             </div>
           </div>
         </div>
-        
+
         <div className="menu__title">Pedicures</div>
         <div className="menu__container">
           <div className="menu__pricelist">
             <div className="menu__pricelist-row  menu__bg-pink">
-              <div className="menu__pricelist-row--title">
-               Pedicure
-              </div>
+              <div className="menu__pricelist-row--title">Pedicure</div>
               <div className="menu__pricelist-row--cost">R110</div>
             </div>
             <div className="menu__pricelist-row ">
-              <div className="menu__pricelist-row--title">
-                Gel Pedicure
-              </div>
+              <div className="menu__pricelist-row--title">Gel Pedicure</div>
               <div className="menu__pricelist-row--cost">R150</div>
             </div>{" "}
           </div>
@@ -63,15 +62,15 @@ const Menu = () => {
         <div className="menu__title">Added services</div>
         <div className="menu__container">
           <div className="menu__pricelist">
-          <div className="menu__pricelist-row ">
+            <div className="menu__pricelist-row ">
               <div className="menu__pricelist-row--title">Gel Overlay</div>
               <div className="menu__pricelist-row--cost">R180</div>
             </div>
-          <div className="menu__pricelist-row  menu__bg-pink">
+            <div className="menu__pricelist-row  menu__bg-pink">
               <div className="menu__pricelist-row--title">Buff & Shine</div>
               <div className="menu__pricelist-row--cost">R110</div>
             </div>
-          <div className="menu__pricelist-row">
+            <div className="menu__pricelist-row">
               <div className="menu__pricelist-row--title">Foot Treatment</div>
               <div className="menu__pricelist-row--cost">R180</div>
             </div>
@@ -85,15 +84,15 @@ const Menu = () => {
         <div className="menu__title">Designs</div>
         <div className="menu__container">
           <div className="menu__pricelist">
-          <div className="menu__pricelist-row">
+            <div className="menu__pricelist-row">
               <div className="menu__pricelist-row--title">Drawings</div>
               <div className="menu__pricelist-row--cost">R5 per nail</div>
-          </div>
+            </div>
 
-          <div className="menu__pricelist-row menu__bg-pink">
+            <div className="menu__pricelist-row menu__bg-pink">
               <div className="menu__pricelist-row--title">Glitter</div>
               <div className="menu__pricelist-row--cost">R10 per nail</div>
-          </div>
+            </div>
           </div>
         </div>
       </div>
