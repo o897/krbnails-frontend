c84acb24a6158c14738c21bc63208c2e87ff5565
o897 <orapelengm239@gmail.com>
2026-06-13 22:00:27 +0200

    rmv title at the top

--- src/components/BookService.jsx
+++ src/components/BookService.jsx
@@ -127,11 +127,11 @@ const BookService = () => {
         
         
         <div className="appointment">
-          <div className="row app-serv-offd">
+          {/* <div className="row app-serv-offd">
           {services.map(({ title }) => 
             <div key={title} className="serv_offered ">{title}</div>
           )}
-        </div>
+        </div> */}
           <div className="appointment__services">
 
             {/* Nail drawings section */}
