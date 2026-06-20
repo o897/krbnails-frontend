ef5aebe8a5959e5c7bbd0f0268f86244e4f3279e
o897 <orapelengm239@gmail.com>
2026-06-20 15:47:58 +0200

    done done update conf pg

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -7,14 +7,15 @@ function Confirmation() {
     <>
       <div className="confirmation">
         <h2>Your Booking has been made</h2>
-        <span><strong>To reserve your booing make a payment within the next hour.</strong></span>
+        <span style={{ textAlign: 'center' , fontWeight: '700'}}>
+          To reserve your booking, make a payment within 10 minutes.
+        </span>        
         <span>
-
-        <strong>Account :</strong> 8468 89894 89465 <br/>
-        <strong>Bank name : </strong>Capitec <br/>
-        <strong>Reference :</strong>  Your contact
+          <strong>Account :</strong> 187 590 8909<br />
+          <strong>Bank name : </strong> Capitec <br />
+          <strong>Reference :</strong> Name + contact no
         </span>
-
+        <p>Immadiate Payments</p>
         <span><strong>PayShap ID : </strong> +27824066527@CAPITEC</span>
         <span>Send your proof of payment to 824065421</span>
         <img className="confirmation-img" src={confirm} alt="" />
