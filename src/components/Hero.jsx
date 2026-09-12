f2710a2c66c7690ea51b85477f6199e9900d0c68
Orapeleng Mathebula <orapelengm239@gmail.com>
2026-09-12 17:13:07 +0200

    update home

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,117 +1,13 @@
-import { useState } from "react";
 import ExperienceShowcase from "./ExperienceShowcase";
-import designImg from "../assets/services/design.jpg";
-import pedicureImg from "../assets/services/krbhome.jpg";
-import acrylicImg from "../assets/services/acrylic.jpeg";
 import { images } from "../data";
 import Navbar from "../components/Navbar"
-import { IoIosArrowDropleft } from "react-icons/io";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./../assets/home.jpg";
 import Footer from "./Footer";
 
 // data-aos="fade-up"
 function Hero() {
-  const [isClicked, setClick] = useState(false);
-  const [selectedService, setSelectedService] = useState("acrylic");
-  const services = ["acrylic", "design", "pedicure", "soaking"];
 
-  const handlePrev = () => {
-    const i = services.indexOf(selectedService);
-    setSelectedService(services[i - 1] || services[services.length - 1]);
-  };
-
-  const handleNext = () => {
-    const i = services.indexOf(selectedService);
-    setSelectedService(services[i + 1] || services[0]);
-  };
-
-  const renderService = () => {
-    switch (selectedService) {
-      case "pedicure":
-        return (
-          <div className="services__container services__container-big">
-            <img src={pedicureImg} alt="" srcSet="" />
-            <div className="services-info">
-              <h2 className="service-title">Pedicure</h2>
-              <div className="services-description">
-                A pedicure is a beauty treatment for the feet and toenails,
-                involving soaking, exfoliating, and moisturizing. It includes
-                trimming and shaping the nails, often with nail polish
-                application.
-              </div>
-              <div className="services-types">
-                <div className="services__sub">Soak & Cleanse | Exfoliation | Nail Care & Cuticle Treatment</div>
-              </div>
-            </div>
-          </div>
-        );
-      case "design":
-        return (
-          <div className="services__container services__container-big">
-            <img src={designImg} alt="" />
-            <div className="services-info">
-              <h2 className="service-title">Designs</h2>
-              <div className="services-description">
-                Elevate your look with stunning nail art designs that showcase
-                your unique style. Our expert artists create vibrant, intricate
-                patterns tailored just for you. Make a bold statement with
-                beautifully crafted, eye-catching nails.
-              </div>
-              <div className="services-types">
-                <div className="services__sub">
-                  Shape & Prep | Base Coat Application | Nail Art & Design
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      case "soaking":
-        return (
-          <div className="services__container services__container-big">
-            <img src={acrylicImg} alt="" srcSet="" />
-            <div className="services-info">
-              <h2 className="service-title">Soaking</h2>
-              <div className="services-description">
-                Soaking nails involves immersing them in warm, soapy water to
-                soften cuticles and prepare them for grooming. This step cleanses
-                and hydrates the nails, making trimming and shaping easier. It
-                also enhances relaxation and overall nail health.
-              </div>
-              <div className="services-types">
-                <div className="services__sub">
-                  Warm Soak | Cuticle Softening | Relaxing Prep
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      case "acrylic":
-        return (
-          <div
-            className="services__container services__container-big"
-            data-aos="fade-right"
-          >
-            <img src={acrylicImg} alt="" srcSet="" />
-            <div className="services-info">
-              <h2 className="service-title">Buff & Shine</h2>
-              <div className="services-description">
-                A pedicure is a beauty treatment for the feet and toenails,
-                involving soaking, exfoliating, and moisturizing. It includes
-                trimming and shaping the nails, often with nail polish
-                application.
-              </div>
-              <div className="services-types">
-                <div className="services__sub">Buff | Shine</div>
-              </div>
-              <div className="services__container">
-              </div>
-            </div>
-          </div>
-        );
-    }
-  };
 
   return (
     <div className="wrapper">
@@ -139,14 +35,14 @@ function Hero() {
 
         <ExperienceShowcase />
 
-        <section>
+        {/* <section>
           <div className="book">
             <h2>Nail Your Look, Every Day!</h2>
             <button className="book__btn">
               <Link to="book">BOOK AN APPOINTMENT</Link>
             </button>
           </div>
-        </section>
+        </section> */}
 
         <section id="gallery" className="gallery">
           <h2 className="gallery__heading">Follow our journey</h2>
@@ -154,10 +50,6 @@ function Hero() {
             <div className="gallery__container">
               <img src={images[1].source} alt="" />
               <img src={images[2].source} alt="" />
-            </div>
-            <div className="gallery__container">
-              <img src={images[3].source} alt="" />
-              <img src={images[4].source} alt="" />
             </div>
             <div className="gallery__container">
               <img src={images[5].source} alt="" />
